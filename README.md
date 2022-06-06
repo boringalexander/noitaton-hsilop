3389239cf67d0c71e37189a318b950d61dbd8bd4
Alexander Boring <alexander.s.boring@gmail.com>
2022-06-06 13:33:19 -0400

    Update README and add santity tests for code_exercise.md examples

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # noitaton-hsilop
-A reverse('noitaton-hsilop') calculator
+noitaton-hsilop is a reverse polish notation calculator for the command line written in Node.js.
+
+It relies heavily on the parser combinator library, *arcsecond*, to 
+parse user input into either semantically valid RPN arithmetic expressions, or other user input commands. 
+
+## Commands
+Aside from basic arithmetic operations, noitaton-hsilop supports viewing the current RPN expression (`:it`), clearing the expression (`c`), and quitting (`q`).
+```
+> 3 3 + 
+5 
+> :it
+3 3 +
+> c
+> :it
+
+> q
+'Goodbye'
+```
+
+
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,34 @@
+'use strict';
+
+const { faker } = require('@faker-js/faker');
+const Token = require('../lib/token');
+const { expressionParser } = require('../lib/parser');
+const { evaluate, calculate } = require('../lib/calculator');
+
 describe('solveRPN', () => {
-    it('is true', () => {
-        expect(true).toBe(true);
-    })
-})
+
+    describe('evaluate', () => {
+
+        it('Given the token value is a binary operation, then it must apply the operation to the stack', () => {
+            const nums = [faker.datatype.number(), faker.datatype.number()];
+            expect(evaluate(nums, Token.addition())).toStrictEqual([nums[0] + nums[1]]);
+        });
+
+        it('Given the token value is for a number, then it must push the number onto the stack', () => {
+
+            const nums = [faker.datatype.number(), faker.datatype.number()];
+            expect(evaluate(nums, Token.number('10'))).toStrictEqual(nums.concat([10]));
+        });
+    });
+
+    describe('calculate', () => {
+
+        it('evaluates tokenized expressions', () => {
+
+            const inputs = ['5 8 +', '5 5 5 8 + + - 13 +', '-3 -2 * 5 +', '5 9 1 - /'];
+            const tokenizedInputs = inputs.map(line => expressionParser.run(line).result);
+            const results = tokenizedInputs.map(calculate);
+            expect(results).toStrictEqual([13, 0, 11, 0.625]);
+        });
+    });
+});
